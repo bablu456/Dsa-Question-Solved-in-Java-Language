9fc81900f451fb9061f0ea20ec4e06d1b521b3cc
bablu kumar <bablu707041dk@gmail.com>
2026-01-02 14:35:03 +0530

    refactor: optimized solution using HashSet for O(n) complexity

--- src/Contains_Duplicate/ContainsDuplicate.java
+++ src/Contains_Duplicate/ContainsDuplicate.java
@@ -3,14 +3,25 @@ package Contains_Duplicate;
 // Given an integer array nums, return true if any value appears at least twice in the array,
 // and return false if every element is distinct.
 
+import java.util.HashSet;
+
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums){
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    return true;
-                }
+//        for(int i=0;i<nums.length;i++){
+//            for(int j=i+1;j<nums.length;j++){
+//                if(nums[i]==nums[j]){
+//                    return true;
+//                }
+//            }
+//        }
+
+        HashSet<Integer> see = new HashSet<>();
+
+        for(int num : nums){
+            if(see.contains(num)){
+                return true;
             }
+            see.add(num);
         }
 
         return false;
