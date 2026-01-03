e440c1884c05a2ab165bb9fc350334c1cee847aa
bablu kumar <bablu707041dk@gmail.com>
2026-01-03 16:07:40 +0530

    feat: implemented sorting solution for anagram check

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -2,10 +2,19 @@
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 // An Anagram is a word or phrase formed by rearranging the letters of a different word.
 
+import java.util.Arrays;
+
 public class ValidAnagram {
     public boolean isAnagram(String s,String t){
+    if(s.length() != t.length()) return false;
 
-        return false;
+    char[] sChars = s.toCharArray();
+    char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars,tChars);
     }
 
 }
