cafe9b9894e58ab939885b11afab3506d383410b
bablu kumar <bablu707041dk@gmail.com>
2026-01-03 16:04:32 +0530

    chore: added boilerplate for leetcode 242 valid anagram

---
+++ src/ValidAnagram.java
@@ -0,0 +1,11 @@
+// LeetCode 242: Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word.
+
+public class ValidAnagram {
+    public boolean isAnagram(String s,String t){
+
+        return false;
+    }
+
+}
