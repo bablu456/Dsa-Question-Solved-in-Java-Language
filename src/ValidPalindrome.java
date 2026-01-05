33746b85f82eb9abc9be474f4837b9ac119a0c1e
bablu kumar <bablu707041dk@gmail.com>
2026-01-05 15:17:45 +0530

    refactor: optimized solution using two pointers O(n) time O(1) space

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,9 +1,31 @@
 public class ValidPalindrome {
     public boolean isPalindrome(String s){
-        String clean = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+//        String clean = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
 
-        String reversed = new StringBuilder(clean).reverse().toString();
+//        String reversed = new StringBuilder(clean).reverse().toString();
 
-        return clean.equals(reversed);
+//        return clean.equals(reversed);
+        int left = 0;
+        int right = s.length() -1;
+        while(left < right){
+            char start = s.charAt(left);
+            char end = s.charAt(right);
+
+            if(!Character.isLetterOrDigit(start)){
+                left++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(end)){
+                right--;
+                continue;
+            }
+            if(Character.toLowerCase(start) != Character.toLowerCase(end)){
+                return  false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
     }
 }
