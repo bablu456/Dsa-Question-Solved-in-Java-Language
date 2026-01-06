890475282a94ea94e01aa5c6007bc98e0e1f6570
bablu kumar <bablu707041dk@gmail.com>
2026-01-06 20:32:00 +0530

    feat: implemented brute force solution O(n^2)

--- src/BestTimeStock.java
+++ src/BestTimeStock.java
@@ -1,6 +1,16 @@
 public class BestTimeStock {
     public int maxProfit(int[] prices){
+        int maxProfit = 0;
 
-        return 0;
+        for(int i=0;i<prices.length;i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
     }
 }
