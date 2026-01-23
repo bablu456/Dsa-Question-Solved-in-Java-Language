03801c1d204acb049c4e3688fd484d368d5e7cb6
bablu kumar <bablu707041dk@gmail.com>
2026-01-23 22:06:38 +0530

    I added the the new code

---
+++ src/GrindingQuestions/ValidParentheses.java
@@ -0,0 +1,42 @@
+package GrindingQuestions;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+
+                if((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter brackets string: ");
+        String s = sc.nextLine();
+
+        boolean result = isValid(s);
+
+        System.out.println("Is Valid: "+result);
+
+        sc.close();
+    }
+}
